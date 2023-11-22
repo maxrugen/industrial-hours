f6935381c2977a8b31197eda06d6b2f93022983e
Max Rugen <max.rugen@personio.de>
2023-11-22 12:28:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Industrial Hours Calculator
+
+This simple web-based Industrial Hours Calculator allows you to convert time durations between different formats – hours and minutes. You can use it to calculate industrial hours for a given time input in either "hh:mm" or minutes format.
+
+## Usage
+
+1. Open Industrial Hours Calculator at [maxrugen.github.io/industrial-hours](https://maxrugen.github.io/industrial-hours) or by running the HTML file locally.
+2. Enter the duration in the input field.
+3. Click the "Calculate" button to convert the input into industrial hours.
+
+## Format
+
+- You can input time in either "hh:mm" format or as minutes.
+- Examples:
+  - Entering "1:30" or "90" will result in 1.5 industrial hours.
+
+## License
+
+This Industrial Hours Calculator is licensed under the [MIT License](LICENSE). Feel free to use, modify, and distribute the code.
+
+## How to Contribute
+
+If you find any issues or have suggestions for improvement, feel free to [open an issue](https://github.com/maxrugen/industrial-hours/issues) or create a pull request. We welcome contributions!
+
+## Disclaimer
+
+This Industrial Hours Calculator is a basic tool and should be used with caution. The author is not responsible for any inaccuracies or problems that may arise from its use.
---
+++ script.js
@@ -0,0 +1,58 @@
+// Function to read time input from the DOM
+function readTimeInput() {
+    return document.getElementById("timeInput").value;
+}
+// Function to display an error message on the DOM
+function displayError(message, resultElement, errorElement) {
+    errorElement.textContent = message;
+    resultElement.textContent = ""; // Clear any previous result
+    resultElement.classList.remove("success"); // Remove success styling
+    errorElement.classList.add("error"); // Apply error styling
+}
+// Function to display the result on the DOM
+function displayResult(result, resultElement, errorElement) {
+    resultElement.textContent = result;
+    errorElement.textContent = ""; // Clear any previous error
+    errorElement.classList.remove("error"); // Remove error styling
+    resultElement.classList.add("success"); // Apply success styling
+}
+// Function to calculate industrial hours from hours and minutes
+function calculateIndustrialHours(hours, minutes) {
+    return hours + minutes / 60;
+}
+// Function to format a number to two decimal places
+function formatToTwoDecimalPlaces(number) {
+    return number.toFixed(2);
+}
+// Function to validate input values
+function validateInput(hours, minutes) {
+    return !isNaN(hours) && !isNaN(minutes) && hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59;
+}
+// Main conversion function
+function convertTime() {
+    const timeInput = readTimeInput();
+    const resultElement = document.getElementById("result");
+    const errorElement = document.getElementById("error");
+    if (timeInput.trim() === "") {
+        displayError("Please enter a duration in the format hh:mm or in minutes.", resultElement, errorElement);
+    } else {
+        errorElement.textContent = "";
+        if (timeInput.includes(":")) {
+            const [hours, minutes] = timeInput.split(":").map(part => parseInt(part));
+            if (validateInput(hours, minutes)) {
+                const decimalHours = calculateIndustrialHours(hours, minutes);
+                displayResult(`Industrial Hours: ${formatToTwoDecimalPlaces(decimalHours)} hours`, resultElement, errorElement);
+            } else {
+                displayError("Invalid entry. Hours must be between 0 and 23, and minutes must be between 0 and 59.", resultElement, errorElement);
+            }
+        } else {
+            const minutes = parseInt(timeInput);
+            if (!isNaN(minutes) && minutes >= 0) {
+                const decimalHours = calculateIndustrialHours(0, minutes);
+                displayResult(`Industrial Hours: ${formatToTwoDecimalPlaces(decimalHours)} hours`, resultElement, errorElement);
+            } else {
+                displayError("Invalid entry. Please enter a valid duration in the format hh:mm or in minutes.", resultElement, errorElement);
+            }
+        }
+    }
+}
